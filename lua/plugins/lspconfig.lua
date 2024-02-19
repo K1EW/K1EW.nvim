623886e9b48bdc25ce0b2df4097e6462a99a304b
K1EW <weerawatwmt@gmail.com>
2024-02-20 00:44:07 +0700

    add lspconfig.lua

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,48 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "clangd",
+                    "html",
+                    "jsonls",
+                    "tsserver",
+                    "lua_ls",
+                    "pyright",
+                    "tailwindcss"
+                }
+            })
+        end
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            local servers = {
+                "clangd",
+                "html",
+                "jsonls",
+                "tsserver",
+                "lua_ls",
+                "pyright",
+                "tailwindcss"
+            }
+            for _, server in ipairs(servers) do
+                lspconfig[server].setup({
+                    on_attach = function()
+                        vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+                        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+                        vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+                    end
+                })
+            end
+        end
+    }
+}
