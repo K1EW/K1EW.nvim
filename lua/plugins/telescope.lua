4d0657bb6c59a06f2610fd13fc5a0f96319f7cdb
K1EW <weerawatwmt@gmail.com>
2024-02-20 00:52:43 +0700

    add code action functionality along with telescope-ui

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,7 +1,8 @@
 return {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-        "nvim-lua/plenary.nvim"
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-ui-select.nvim"
     },
     config = function()
         local actions = require("telescope.actions")
@@ -14,8 +15,15 @@ return {
                         ["<C-k>"] = actions.move_selection_previous
                     }
                 }
+            },
+            extentions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown()
+                }
             }
         })
+
+        require("telescope").load_extension("ui-select")
 
         local builtin = require("telescope.builtin")
         local options = { noremap = true, silent = true }
