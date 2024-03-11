e8293ee3b1124e063d1d42b297e60226da11e38f
K1EW <weerawatwmt@gmail.com>
2024-03-12 01:24:52 +0700

    bind <Leader>gi: go to implementation

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -43,6 +43,7 @@ return {
         lspconfig.cssls.setup({ capabilities = capabilities })
 
         vim.keymap.set("n", "<Leader>gd", vim.lsp.buf.definition, {})
+        vim.keymap.set("n", "<Leader>gi", vim.lsp.buf.implementation, {})
         vim.keymap.set("n", "<Leader>gr", vim.lsp.buf.references, {})
         vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
         vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, {})
